703e0c2d1a6e351d07be435349539ff7b26d5b49
Velocitypulse <velocitypulse@outlook.fr>
2015-12-29 13:16:17 +0100

    save

--- lib/ft_strnew.c
+++ lib/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <cchameyr@students.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 12:10:32 by cchameyr          #+#    #+#             */
-/*   Updated: 2015/11/29 15:23:58 by cchameyr         ###   ########.fr       */
+/*   Updated: 2015/12/23 12:06:15 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ char	*ft_strnew(size_t size)
 
 	str = (char *)malloc(sizeof(char) * (size + 1));
 	if (str)
-		return (ft_memset(str, 0, size));
+		return (ft_memset(str, 0, size + 1));
 	return (NULL);
 }
