647e6a5b05a2a3d35cbacaa32526b5d60dbb8cd4
Clement CHAMEYRAT <velocitypulse@outlook.fr>
2016-09-21 12:38:35 +0200

    debug de ft_strisdigit

--- ft_strisdigit.c
+++ ft_strisdigit.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/06/16 16:16:37 by cchameyr          #+#    #+#             */
-/*   Updated: 2016/09/21 11:56:00 by cchameyr         ###   ########.fr       */
+/*   Updated: 2016/09/21 12:36:43 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		ft_strisdigit(char *str)
 	{
 		if ((str[i] >= 48 && str[i] <= 57) && end == 0)
 			i++;
-		else if (str[i] == ' ')
+		else if (str[i] == ' ' && end == 0)
 			end = 1;
 		else
 			return (0);
