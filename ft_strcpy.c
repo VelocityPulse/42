a060df0eb4760c5c144dce2af5dec8eba9332488
velocitypulse <velocitypulse@outlook.fr>
2017-08-08 12:15:26 +0200

    opti ft_strcpy

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: cchameyr <cchameyr@students.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 14:58:10 by cchameyr          #+#    #+#             */
-/*   Updated: 2015/11/25 23:27:02 by cchameyr         ###   ########.fr       */
+/*   Updated: 2017/08/08 12:14:17 by cchameyr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,9 @@ char	*ft_strcpy(char *dst, const char *src)
 {
 	int i;
 
-	i = 0;
-	while (src[i])
-	{
+	i = -1;
+	while (src[++i])
 		dst[i] = src[i];
-		i++;
-	}
 	dst[i] = '\0';
 	return (dst);
 }
